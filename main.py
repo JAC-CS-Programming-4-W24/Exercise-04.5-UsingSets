bb7d5fc4a77e8ce8f2185cbd593bf6fd5a63132e
Ian Clement <ian.clement@johnabbott.qc.ca>
2024-02-19 09:06:48 -0500

    Starter

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+def main():
+    web1: set[str] = {"Allison", "Harold", "Marina", "Jayce", "Sydney", "Princeton", "Hunter", "Alonso", "Molly",
+                       "Sylas", "Sierra", "Izaiah", "Gloria", "Felipe", "Aleena", "Ares", "Elizabeth", "Matteo",
+                       "Harmoni", "Leif", "Kendra", "Ezequiel", "Amaia", "Jakob", "Drew", "Conner", "Eva", "Emerson"}
+    p3: set[str] = {"Allison", "Marina", "Jayce", "Sydney", "Princeton", "Hunter", "Alonso", "Sylas", "Sierra",
+                     "Izaiah", "Gloria", "Felipe", "Aleena", "Ares", "Elizabeth", "Matteo", "Harmoni", "Kendra",
+                     "Ezequiel", "Amaia", "Jakob", "Drew", "Conner", "Eva", "Emerson"}
+    db: set[str] = {"Allison", "Harold", "Marina", "Jayce", "Princeton", "Hunter", "Alonso", "Molly", "Sylas",
+                     "Sierra", "Izaiah", "Gloria", "Aleena", "Ares", "Elizabeth", "Matteo", "Harmoni", "Leif",
+                     "Ezequiel", "Amaia", "Jakob", "Drew", "Conner", "Eva", "Emerson"}
+    os: set[str] = {"Allison", "Harold", "Marina", "Jayce", "Sydney", "Princeton", "Hunter", "Alonso", "Molly",
+                     "Sylas", "Sierra", "Izaiah", "Gloria", "Felipe", "Aleena", "Ares", "Elizabeth", "Matteo",
+                     "Harmoni", "Leif", "Kendra", "Ezequiel", "Amaia", "Jakob", "Drew", "Conner", "Eva", "Emerson"}
+
+    # You can write solutions here, or you can use a python REPL to solve these. For REPL we
+    # recommend ipython and it's easy to setup:
+
+    #   0. Check that you have a python interpreter configured (Bottom bar, right side).
+    #   1. Open the pycharm integrated terminal (look for Terminal on the Left sidebar), or Alt-F12 on macOS.
+    #   2. Install ipython in your virtual environment:
+    #       > pip install ipython
+    #       > ipython
+    #   3. Copy/paste the four sets above in the REPL, hit enter and have fun!
+
+
+    # TODO Q1: Is it true that Alonzo, Sylas and Kendra are in "os"?
+
+    # TODO Q2: How many students are taking at least one of these classes?
+
+    # TODO Q3: Which students are in "web1" but not in either "p3" or "db"?
+
+    # TODO Q4: How many students are taking all four classes this semester?
+
+if __name__ == '__main__':
+    main()
